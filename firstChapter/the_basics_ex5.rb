7c9c79a35217aedee540964907696b07f6a291b1
samuel gustave <gustavesam@gmail.com>
2015-08-16 17:28:46 -0500

    change in exercice 5 define factorial method

--- firstChapter/the_basics_ex5.rb
+++ firstChapter/the_basics_ex5.rb
@@ -1,4 +1,19 @@
+# manually calculate factorial
 puts 5 * 4 * 3 * 2 * 1
 puts 6 * 5 * 4 * 3 * 2 * 1
 puts 7 * 6 * 5 * 4 * 3 * 2 * 1
 puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# There is another way more elegant !!!
+# define a factorial method
+def factorial(x)
+	res = 1
+	# use a block that multiply the variable res by each value of the range
+	(1..x).each { |i| res *= i }
+	return res
+end
+
+puts "----------"
+
+# for each number of the range print result using factorial method
+(5..8).each { |n| puts factorial(n)}
